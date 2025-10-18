c54710503685b3af10c7f47d12d04da139f0c1f5
Neil Bramley <nrbramley@gmail.com>
2025-10-18 15:08:15 +0100

    partway through updates

--- js/exp2/static/js/task.js
+++ js/exp2/static/js/task.js
@@ -1151,3 +1151,12 @@ function tutorialText(step) {
     }
     document.getElementById("tutText").innerHTML = tutText;
 }
+
+
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
