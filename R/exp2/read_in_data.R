7a67c8eea00d400e2fd9942e9ddabb7280ed4952
Neil Bramley <nrbramley@gmail.com>
2025-11-07 10:50:50 +0000

    pilot results

--- R/exp2/read_in_data.R
+++ R/exp2/read_in_data.R
@@ -1,5 +1,6 @@
 library(RMySQL)
 library(rjson)
+library(tidyverse)
 
 rm(list=ls())
 
@@ -18,6 +19,8 @@ mydb = dbConnect(RMySQL::MySQL(),
 dbListTables(mydb)
 df.raw<-dbReadTable(mydb,"hexcraft_exp2")
 
+df.raw<-df.raw[-c(1:3),] #Remove test data points from Neil and Haozhe
+
 # df.raw<-df.raw[2,]
 vec<-rep(NA, nrow(df.raw))
 df.sw<-data.frame(p_ix=vec,
@@ -26,7 +29,7 @@ df.sw<-data.frame(p_ix=vec,
                   gender=vec,
                   age=vec,
                   ins_time=vec,tut_time=vec,trial_time=vec,total_time=vec,
-                  comprehension_score=vec,
+                  comprehension_score=vec, tutorial_attempts=vec, tutorial_keys = vec,
                   t1_solved=vec,t2_solved=vec,t3_solved=vec,t4_solved=vec,
                   t5_solved=vec,t6_solved=vec,t7_solved=vec,t8_solved=vec,
                   t1_attempts=vec,t2_attempts=vec,t3_attempts=vec,t4_attempts=vec,
@@ -43,6 +46,8 @@ df.tw<-data.frame(p_ix=c(),
                   attempt=c(),
                   actions=c(),
                   success=c())
+
+ls.states<-list()
 
 for (ppt in 1:nrow(df.raw))
 {
@@ -85,7 +90,7 @@ for (ppt in 1:nrow(df.raw))
     }
 
   }
-  df.sw$comments[ppt]<-ls.p$subjectwise$prolific_id
+  df.sw$comments[ppt]<-ls.p$subjectwise$comments
  df.sw$n_add[ppt]<-str_count(paste0(df.tw$keycodes[df.tw$p_ix==df.sw$p_ix[ppt]], collapse=''),'A')
  df.sw$n_delete[ppt]<-str_count(paste0(df.tw$keycodes[df.tw$p_ix==df.sw$p_ix[ppt]], collapse=''),'D')
  df.sw$n_corner[ppt]<-str_count(paste0(df.tw$keycodes[df.tw$p_ix==df.sw$p_ix[ppt]], collapse=''),'Z')
@@ -100,9 +105,48 @@ for (ppt in 1:nrow(df.raw))
  
  df.sw$m_steps_successes[ppt]<-mean(nchar(paste0(df.tw$keycodes[df.tw$p_ix==df.sw$p_ix[ppt]]))[df.tw$success[df.tw$p_ix==df.sw$p_ix[ppt]]])
  df.sw$m_steps_failures[ppt]<-mean(nchar(paste0(df.tw$keycodes[df.tw$p_ix==df.sw$p_ix[ppt]]))[!df.tw$success[df.tw$p_ix==df.sw$p_ix[ppt]]])
+ 
+ 
+ # And let's extract the larger and less important items:
+ ls.states[[ppt]]<-list()
+  for (trial in 1:length(ls.p$state_history_tests))
+  {
+    ls.states[[ppt]][[trial]]<-list()
+    for (attempt in 1:length(ls.p$state_history_tests[[trial]]))
+    {
+      ls.states[[ppt]][[trial]][[attempt]]<-list()
+      for (step in 1:length(ls.p$state_history_tests[[trial]][[attempt]]))
+      {
+        ls.states[[ppt]][[trial]][[attempt]][[step]]<-matrix(unlist(ls.p$state_history_tests[[trial]][[attempt]][[step]]), ncol=9)
+      }
+    }
+  }
+ 
+ this_ppt_tutorial_attempts<-this_ppt_tutorial_keys<-rep(NA, length(ls.p$tutorials))
+ for (tutorial in 1:length(ls.p$tutorials))
+ {
+    this_ppt_tutorial_attempts[tutorial]<-length(ls.p$tutorials[[tutorial]])
+    this_ppt_tutorial_keys[tutorial]<-''
+    for (attempt in 1:length(ls.p$tutorials[[tutorial]]))
+    {
+      this_ppt_tutorial_keys[tutorial]<-paste0(this_ppt_tutorial_keys[tutorial], paste0(sapply(ls.p$tutorials[[tutorial]][[attempt]]$keys, '[[', 'key'), collapse=''))
+    }
+ }
+ df.sw$tutorial_attempts[ppt]<-paste0(this_ppt_tutorial_attempts, collapse=',')
+ df.sw$tutorial_keys[ppt]<-paste0(nchar(this_ppt_tutorial_keys), collapse=',')
 }
 
-df.sw<- df.sw %>% mutate(total_solved = t1_solved+t2_solved+t3_solved+t4_solved+t5_solved+t6_solved+t7_solved+t8_solved)
+df.sw<- df.sw %>% mutate(total_solved = t1_solved+t2_solved+t3_solved+t4_solved+t5_solved+t6_solved+t7_solved+t8_solved,
+                         bonus = total_solved*0.2)
 
 df.sw
 df.tw
+
+
+save(file='../dat/exp2.rdata', df.sw, df.tw)
+
+
+for (ppt in 1:nrow(df.raw))
+{
+ls.p[[ppt]]
+}
